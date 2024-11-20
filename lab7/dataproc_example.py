658dab948b2a0f346c894a2c4a7955e7a68e2dbe
maxde18 <m.r.deleeuw@tilburguniversity.edu>
2024-11-20 11:46:12 +0100

    Update dataproc_example.py
    personalized vars

--- lab7/dataproc_example.py
+++ lab7/dataproc_example.py
@@ -9,7 +9,7 @@ spark = SparkSession.builder.config(conf=sparkConf).getOrCreate()
 # Load data from BigQuery.
 df = spark.read \
   .format("bigquery") \
-  .load("your_project_id.labdataset.retaildata")    # project_id.datatset.tablename. Use your project id
+  .load("de2024-435320.labdataset.retaildata")    # project_id.datatset.tablename. Use your project id
 df.printSchema()
 df.show(4)
 words = df.where(df.Country == "France")
@@ -25,7 +25,7 @@ ordered_word_count = words.groupby(words.word).count().orderBy(col("count").desc
 print(ordered_word_count.count())
 ordered_word_count.show(100)
 # Use the Cloud Storage bucket for temporary BigQuery export data used by the connector.
-bucket = "temp_de2024"  # use your bucket 
+bucket = "temp_de2024_708179"  # use your bucket 
 spark.conf.set('temporaryGcsBucket', bucket)
 # Setup hadoop fs configuration for schema gs://
 conf = spark.sparkContext._jsc.hadoopConfiguration()
@@ -33,6 +33,6 @@ conf.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
 conf.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
 # Saving the data to BigQuery
 ordered_word_count.write.format('bigquery') \
-  .option('table', 'your_project_id.labdataset.wordcounts') \
+  .option('table', 'de2024-435320.labdataset.wordcounts') \
   .mode("append") \
   .save()
